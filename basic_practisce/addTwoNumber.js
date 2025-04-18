137f5be57614ad91b195e31d56d682176f2ca6aa
Aditya Mohan <mohanadityajha@gmail.com>
2025-04-18 15:56:46 +0530

    AddTwoNumber

---
+++ basic_practisce/addTwoNumber.js
@@ -0,0 +1,8 @@
+function addTwoNumber(a,b) {
+return a + b 
+
+}
+console.log(addTwoNumber("600", 800)) // 600800
+console.log(addTwoNumber(600, "800")) // 600800
+console.log(addTwoNumber("600", "800")) //600800
+console.log(addTwoNumber(600, 800))  // 1400
