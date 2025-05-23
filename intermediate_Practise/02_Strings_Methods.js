1ad49d15a84c2bd695f18c3c236ec506e8903be8
Aditya Mohan <mohanadityajha@gmail.com>
2025-05-23 18:11:43 +0530

    String Methods

---
+++ intermediate_Practise/02_Strings_Methods.js
@@ -0,0 +1,82 @@
+// JavaScript String Length
+// let text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// const result = text.length;
+// console.log(result);
+
+// The at(position) Method
+// let text = "HELLO WORLD";
+// const result = text.at(6)
+// console.log(result)
+
+//JavaScript String charAt()
+// let text = "HELLO WORLD";
+// const result = text.charAt(6)
+// console.log(result)
+
+
+//JavaScript String charCodeAt()
+// let text = "HELLO WORLD";
+// const result = text.charCodeAt(1)
+// console.log(result)
+
+// JavaScript String slice()
+// let text = "Apple, Banana, Kiwi";
+// const result = text.slice(0,13)
+// console.log(result)
+
+// let text = "Apple, Banana, Kiwi";
+// let part = text.slice(7);
+// console.log(part);
+
+// let text = "Apple, Banana, Kiwi";
+// let part = text.slice(-12,0)
+// console.log(part)
+
+// JavaScript String substring()
+// let str = "Apple, Banana, Kiwi";
+// let part = str.substring(7, 13);
+// console.log(part)
+
+// JavaScript String concat()
+// let text1 = "Hello";
+// let text2 = "World";
+// let text3 = text1.concat(" ",text2);
+// console.log(text3)
+
+// JavaScript String trim()
+// let text1 = "      Hello World!      ";
+// let text2 = text1.trim();
+// console.log(text2)
+
+// JavaScript String toUpperCase()
+// let text1 = "Hello World!";
+// let text2 = text1.toUpperCase();
+// console.log(text2);
+
+// JavaScript String toLowerCase()
+// let text1 = "Hello World!";       
+// let text2 = text1.toLowerCase();
+// console.log(text2)
+
+// JavaScript String repeat()
+// let text = "Hello world!";
+// let result = text.repeat(5)
+// console.log(result)
+
+// Replacing String Conten
+// let text = "Please visit Microsoft!";
+// const result = text.replace("Microsoft","W3Schools")
+// console.log(result )
+
+// JavaScript String ReplaceAll()
+// let text = text.replaceAll("Cats","Dogs");
+// let text1 = text.replaceAll("cats","dogs");
+// console.log(text1)
+
+
+// JavaScript String split()
+let str = "Apple, Banana, Kiwi";
+let res = str.split("@")
+console.log(res);
+
+
