2d1803ebb0bfd7bf223275d98060aacb55494e24
Aditya Mohan <mohanadityajha@gmail.com>
2025-05-23 18:10:19 +0530

    Sorting and String

---
+++ intermediate_Practise/01_Strings.js
@@ -0,0 +1,31 @@
+// let text =
+// `The quick
+// brown fox
+// jumps over
+// the lazy dog`;
+// //console.log(text)
+
+// let text2 = "The quick
+// brown fox
+// jumps over
+// the lazy dog"
+// console.log(text2)
+
+
+// let x = new String("John");
+// let y = new String("John");
+// console.log(x===y);
+
+// let y = new String("John");
+// console.log(typeof y)
+
+// let x = "John";
+// let y = new String("John");
+// console.log(x===y);
+
+// let x = new String("John");
+// let y = new String("John");
+// console.log(x==y)
+
+let text = "We are the so-called \"Vikings\" from the north.";
+console.log(text)
--- intermediate_Practise/sorting.js
+++ intermediate_Practise/sorting.js
@@ -1,5 +1,5 @@
 arr = [2,4,6,8,10,9,7,5,3,1]
 arr.sort(function(a,b){
-{return a - b }
+return a - b 
 })
 console.log(arr)
