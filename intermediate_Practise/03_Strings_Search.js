ddbbd6ef93271b00f9cd41a3a03ecfe90d1f0833
Aditya Mohan <mohanadityajha@gmail.com>
2025-05-30 18:40:33 +0530

    Strings-Search

---
+++ intermediate_Practise/03_Strings_Search.js
@@ -0,0 +1,65 @@
+// let text = "The rain in SPAIN stays mainly in the plain";
+// let res = text.match("ain");
+// console.log(res)
+
+
+// let text = "The rain in SPAIN stays mainly in the plain";
+// let res = text.match("ain");
+// console.log(res);
+
+
+// const iterator = text.matchAll("Cats");
+// console.log(iterator);
+
+// let text = "Helloworld, welcome to the universe.";
+// const res = text.startsWith("world", 5)
+// console.log(res);
+
+
+// let firstName = "John";
+// let lastName = "Doe";
+
+// let text = `Welcome ${firstName}, ${lastName}!`;
+// console.log(text);
+
+
+// JavaScript String indexOf()
+// let text = "Please locate where 'locate' occurs!";
+// let res = text.indexOf("locate");
+// console.log(res);
+
+//JavaScript String lastIndexOf()
+// let text = "Please locate where 'locate' occurs!";
+// let res = text.lastIndexOf("locate")
+// console.log(res)
+
+//JavaScript String search()
+// let text = "Please locate where 'locate' occurs!";
+// let res = text.search("locate")
+// console.log(res)
+
+// let text = "Please locate where 'locate' occurs!";
+// const res = text.search(/locate/);
+// console.log(res)
+
+
+// let text = "The rain in SPAIN stays mainly in the plain";
+// const res = text.match("ain");
+// console.log(res);
+
+
+//JavaScript String includes()
+// let text = "Hello world, welcome to the universe.";
+// console.log(text.includes("universes"));
+
+
+// JavaScript String startsWith()
+// let text = "Hello world, welcome to the universe.";
+// const res = text.startsWith("Hello")
+// console.log(res)
+
+// JavaScript String endsWith()
+let text = "Hello world, welcome to the universe.";
+const res = text.endsWith("universe")
+console.log(res);
+
