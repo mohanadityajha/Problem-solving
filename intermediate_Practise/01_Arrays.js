37645eb6d8f69b5e7926c21351d983edec961f4b
Aditya Mohan <mohanadityajha@gmail.com>
2025-06-02 18:08:56 +0530

    Arrays

---
+++ intermediate_Practise/01_Arrays.js
@@ -0,0 +1,46 @@
+// Creating an Array
+// const cars = ["Toyota", "Honda", "Ford", "Chevrolet"];
+// console.log(cars);
+
+//Using the JavaScript Keyword new
+// const cars = new Array("Saab", "Volvo", "BMW");
+// console.log(cars);
+
+// Accessing Array Elements
+// const cars = ["Saab", "Volvo", "BMW"];
+// let car = cars[0];
+// console.log(car)
+
+// Changing an Array Element
+// const cars = ["Saab", "Volvo", "BMW"];
+// cars[0] = "Opel";
+// console.log(cars);
+
+// Converting an Array to a String
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// const fruit= fruits.toString()
+// console.log(fruit);
+
+//Access the Full Array
+// const cars = ["Saab", "Volvo", "BMW"];
+// console.log(cars)
+
+// Arrays are Objects
+// const person = {firstName:"John", lastName:"Doe", age:46};
+// console.log(person)
+
+// The length Property
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// let fruit = fruits.length;
+// console.log(fruit);
+
+//Accessing the Last Array Element
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// const fruit = fruits[fruits.length-1]
+// console.log(fruit);
+
+
+// Adding Array Elements
+const fruits = ["Banana", "Orange", "Apple"];
+const res = fruits.push("Mango");
+console.log(res)
