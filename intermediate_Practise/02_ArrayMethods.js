3843845e257b0d2f57da4dbda4076e6b93382243
Aditya Mohan <mohanadityajha@gmail.com>
2025-06-03 14:25:51 +0530

    Array-Search

---
+++ intermediate_Practise/02_ArrayMethods.js
@@ -0,0 +1,80 @@
+// JavaScript Array length
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// const res = fruits.length;
+// console.log(res)
+
+
+//JavaScript Array toString
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// const res = fruits.toString()
+// console.log(res);
+
+//JavaScript Array at()
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// const res = fruits.at(3)
+// console.log(res);
+
+//JavaScript Array join
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// const res = fruits.join("_")
+// console.log(res);
+
+// JavaScript Array pop
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// const res = fruits.pop()
+// console.log(res)
+// console.log(fruits)
+
+// JavaScript Array push
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// const res = fruits.push("kiwi")
+// console.log(res);
+// console.log(fruits)
+
+// JavaScript Array shift()
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// const res = fruits.shift()
+// console.log(res)
+// console.log(fruits)
+
+// JavaScript Array unshift()
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// const res = fruits.unshift("kiwi")
+// console.log(res);
+// console.log(fruits);
+
+// JavaScript Array concat()
+// const arr1 = ["Cecilie", "Lone"];
+// const arr2 = ["Emil", "Tobias", "Linus"];
+// const arr3 = ["Robin", "Morgan"];
+// const res = arr1.concat(arr2, arr3);
+// console.log(res);
+
+// Array copyWithin()
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// const res = fruits.copyWithin(2, 0);
+// console.log(res)
+// console.log(fruits);
+
+// JavaScript Array fill()
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// const res = fruits.fill("kiwi", 1, 3);
+// console.log(res)
+// console.log(fruits);
+
+//JavaScript Array flat()
+// const myArr = [[1,2],[3,4],[5,6]];
+// const res = myArr.flat();
+// console.log(res);
+// console.log(myArr);
+
+// JavaScript Array splice()
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// const res = fruits.splice(1,2,"kiwi","peach")
+// console.log(res);
+// console.log(fruits)
+
+//JavaScript Array slice()
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+const citrus = fruits.slice(1);
+console.log(citrus);
