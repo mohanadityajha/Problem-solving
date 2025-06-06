d992b3f1b3a334444cd09469b7c549ee687ce712
Aditya Mohan <mohanadityajha@gmail.com>
2025-06-06 21:58:10 +0530

    Logical_Operators

---
+++ intermediate_Practise/01_LogicalOperators.js
@@ -0,0 +1,73 @@
+// 1. What will be the output of true && false?
+//Answer : false
+
+// 2. What will be the output of true || false?
+//Answer : true
+
+// 3. What will be the output of !true?
+//Answer : false
+
+// 4. What will be the output of !false?
+//Answer : true  
+
+// 5. What will be the output of true && true || false?
+//Answer : true
+
+// 6. What will be the output of false || true && false?
+//Answer : false
+
+// 7. What will be the output of (true || false) && (false || true)?
+//Answer : true
+
+// 8. What will be the output of (true && false) || (false && true)?
+//Answer : false
+
+// 9. What will be the output of !(true && false) || (false || true)?
+//Answer : true
+
+// 10. What will be the output of (true || false) && !(false && true)?
+//Answer : true 
+
+// 11. What will be the output of (false || true) && (true || false)?
+//Answer : true 
+
+// 12. What will be the output of !(true || false) && (false || true)?
+//Answer : false
+
+// 13. What will be the output of (true && true) || (false && false)?
+//Answer : true
+
+
+// 14 What will be the output of true && false?
+//Answer : false
+
+// 15 What is the difference between && and || in JavaScript?
+// && (AND) returns true if both operands are true, otherwise false.
+// || (OR) returns true if at least one operand is true, otherwise false.
+
+// 16 Explain the output: false || (true && false)
+// Answer : false
+
+// 17 What is the output of !true?
+// Answer : false
+
+// 18 What is the output of !false?
+// Answer : true
+
+
+// 19 What is the output of true && true || false?
+// Answer : true
+
+// 20 How does || behave with non-boolean values like:
+       console.log(0 || "Hello")
+// Answer : 
+
+// 21 What will this return: "Hello" && 123 && true?
+// Answer : 
+
+// 22. What is the difference between !false and Boolean(false)?
+// Answer : !false returns true, while Boolean(false) returns false. The former negates the value, while the latter converts it to a boolean.
+
+// 23 What is short-circuit evaluation in logical operators?
+// Answer: Short-circuit evaluation means that in an expression using && or ||, evaluation stops as soon as the result is determined. For &&, if the first operand is false, the second operand is not evaluated. For ||, if the first operand is true, the second operand is not evaluated.
+
