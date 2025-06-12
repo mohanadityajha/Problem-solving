e03d344c998976435fed659056d22fd02bc64747
Aditya Mohan <mohanadityajha@gmail.com>
2025-06-12 21:29:11 +0530

    PrimeNumber

---
+++ CodingQuestion/EvenOddNumber.js
@@ -0,0 +1,12 @@
+function findEvenOddNumber(num) {
+    if (num % 2 === 0) {
+
+        return "It is  the Even Number"
+    } else {
+
+        return "It is Odd Number"
+    }
+
+
+}
+console.log(findEvenOddNumber(17));
